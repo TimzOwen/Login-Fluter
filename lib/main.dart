42539217761e7b034919b4a6707458227a1dc431
TimzOwen <timzowen1130@gmail.com>
2019-02-08 15:59:04 -0500

    simple login  for girls get geeky N?auru

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(home: new HomePage(), routes: <String, WidgetBuilder>{
+    "/SecondPage": (BuildContext context) => new SecondPage()
+  }));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+          title: new Text("Home"), backgroundColor: Colors.deepOrange),
+      body: new Container(
+        child: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new IconButton(
+                icon: new Icon(Icons.favorite, color: Colors.redAccent),
+                iconSize: 90.0,
+                onPressed: () {
+                  Navigator.of(context).pushNamed("/SecondPage");
+                },
+              ),
+              new Text("Wan to go For Valentines?"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+          title: new Text("Second Page"), backgroundColor: Colors.deepOrange),
+      body: new Container(
+        child: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new IconButton(
+                icon: new Icon(Icons.home, color: Colors.blueAccent),
+                iconSize: 90.0,
+                onPressed: null,
+              ),
+              new Text("Sorry, Stay at Home and Code "),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
